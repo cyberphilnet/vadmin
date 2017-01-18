3ee7adf3c2492dd96e0258bf60a4feaaf62a4147
Philton Nengobela <philton@vibrantmedia.co.za>
2017-01-18 11:42:53 +0200

    Updated mysql DB connection

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '188.226.217.55'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'frontfoot'),
+            'username' => env('DB_USERNAME', 'vadmin'),
+            'password' => env('DB_PASSWORD', 'Vadmin3398'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
